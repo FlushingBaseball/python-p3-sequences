e6919a482c7dc298a74236c16e202b3cc5213329
Sam Peck <fullstacksam@protonmail.com>
2023-06-13 14:37:02 -0400

    awful awful lesson

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print([])
+    elif length == 1:
+        print([0])
+    else:
+         sequence =[0,1]
+         while len(sequence) < length:
+             next_value = sequence[len(sequence) -1] + sequence[len(sequence) -2]
+             sequence.append(next_value)
+         print(sequence)
+
+   
